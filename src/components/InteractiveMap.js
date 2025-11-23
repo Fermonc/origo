facc0ed9f63f2bcd8631f42426737a6b640ab7c8
OrigoDev <dev@origo.com>
2025-11-22 20:15:02 -0500

    chore: host leaflet assets locally to remove console warnings

--- src/components/InteractiveMap.js
+++ src/components/InteractiveMap.js
@@ -249,8 +249,8 @@ export default function InteractiveMap({ properties }) {
           {/* User Location Marker */}
           {userLocation && (
             <Marker position={userLocation} icon={new L.Icon({
-              iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-              shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+              iconUrl: '/leaflet/marker-icon-2x-red.png',
+              shadowUrl: '/leaflet/marker-shadow.png',
               iconSize: [25, 41],
               iconAnchor: [12, 41],
               popupAnchor: [1, -34],
